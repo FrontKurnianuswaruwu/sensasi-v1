703daa0404d8f196cd32ca5fa30ecd9b85779254
Frontment Waruwu <front.m.k.w@gmail.com>
2026-06-11 10:18:28 +0700

    [UPD] Logout

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -59,7 +59,7 @@ class AuthController extends Controller
         $request->session()->invalidate();
         $request->session()->regenerateToken();
 
-        return redirect()->route('auth.login');
+        return redirect()->route('login');
     }
 
     public function register()
